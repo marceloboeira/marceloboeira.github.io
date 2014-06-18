a4dd80f63e7b783b7aaa9de6352206c324db5416
Marcelo Boeira <marceloboeira@live.com>
2014-06-18 11:40:44 -0300

    Fixing some initial stuff...

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,66 +1,40 @@
-
-$(function() {
-
-
+﻿$(function() {
     setTimeout(function() {
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
 	 }, 100);
-
-
-
    $('.smoothscroll').on('click',function (e) {
 	    e.preventDefault();
-
 	    var target = this.hash,
 	    $target = $(target);
-
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
 	    }, 800, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
-
-
-
 	var sections = $("section");
 	var navigation_links = $("#nav-wrap a");
-
 	sections.waypoint({
-
       handler: function(event, direction) {
-
 		   var active_section;
-
 			active_section = $(this);
 			if (direction === "up") active_section = active_section.prev();
-
 			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
-
 		},
 		offset: '35%'
-
 	});
-
-
-
    $('header').css({ 'height': $(window).height() });
    $(window).on('resize', function() {
-
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
    });
-
-
 
    $(window).on('scroll', function() {
 	var h = $('header').height();
 	var y = $(window).scrollTop();
     var nav = $('#nav-wrap');
-
 	if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
 		nav.fadeOut('fast');
 	}
@@ -72,17 +46,14 @@ $(function() {
         nav.addClass('opaque').fadeIn('fast');
       }
     }
-
 	});
 
     $('.item-wrap a').magnificPopup({
-
        type:'inline',
        fixedContentPos: false,
        removalDelay: 200,
        showCloseBtn: false,
        mainClass: 'mfp-fade'
-
     });
 
     $(document).on('click', '.popup-modal-dismiss', function (e) {
@@ -102,16 +73,12 @@ $(function() {
       randomize: false,
    });
 
-
    $('form#contactForm button.submit').click(function() {
-
       $('#image-loader').fadeIn();
-
       var contactName = $('#contactForm #contactName').val();
       var contactEmail = $('#contactForm #contactEmail').val();
       var contactSubject = $('#contactForm #contactSubject').val();
       var contactMessage = $('#contactForm #contactMessage').val();
-
       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
                '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
       $.ajax({
@@ -125,7 +92,6 @@ $(function() {
                $('#contactForm').fadeOut();
                $('#message-success').fadeIn();   
             }
-            
             else {
                $('#image-loader').fadeOut();
                $('#message-warning').html(msg);
